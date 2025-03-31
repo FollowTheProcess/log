f07f1470414d340d91a8f1c854c3c5dc2927bc7c
Tom Fleet <tomfleet2018@gmail.com>
2025-03-31 18:19:06 +0100

    Add an incredibly basic initial implementation of a logger

---
+++ level.go
@@ -0,0 +1,60 @@
+package log
+
+// Level is a log level.
+type Level int
+
+const (
+	// LevelDebug is the debug log level, intended for verbose logging modes or internal debugging.
+	LevelDebug Level = -4
+
+	// LevelInfo is the info log level, this is the default level, intended for progress updates
+	// and other informational messages.
+	LevelInfo Level = 0
+
+	// LevelWarn is the warning log level, intended for raising recoverable issues to the user. Warning
+	// logs should refer to events that are worth flagging to the user but the program can easily
+	// recover from such as a missing configuration file when the application can fall back to defaults.
+	LevelWarn Level = 4
+
+	// LevelError is the error log level. This is the highest log level provided by log and is intended
+	// for signalling non-recoverable errors to the user. Typically followed by an actual go error and
+	// possibly program exit.
+	LevelError Level = 8
+)
+
+const (
+	debugString = "DEBUG"
+	infoString  = "INFO"
+	warnString  = "WARN"
+	errorString = "ERROR"
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return debugString
+	case LevelInfo:
+		return infoString
+	case LevelWarn:
+		return warnString
+	case LevelError:
+		return errorString
+	default:
+		return "unknown"
+	}
+}
+
+func (l Level) styled() string {
+	switch l {
+	case LevelDebug:
+		return debugStyle.Text(debugString)
+	case LevelInfo:
+		return infoStyle.Text(infoString)
+	case LevelWarn:
+		return warnStyle.Text(warnString)
+	case LevelError:
+		return errorStyle.Text(errorString)
+	default:
+		return "unknown"
+	}
+}
--- log.go
+++ log.go
@@ -1,7 +1,127 @@
-// Package log is a placeholder for something cool.
+// Package log provides a Logger primarily intended for terminal based command line programs.
+//
+// The logs it produces are semi-structured with key value pairs being formatted as key=value but are primarily
+// intended to be human readable and easy on the eye with a good choice of colours, ideal for command line
+// applications that have a --debug or --verbose flag that enables extra logging.
 package log
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello log"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/FollowTheProcess/hue"
+)
+
+// Styles.
+const (
+	timestampStyle = hue.BrightBlack
+	debugStyle     = hue.Blue | hue.Bold
+	infoStyle      = hue.Cyan | hue.Bold
+	warnStyle      = hue.Yellow | hue.Bold
+	errorStyle     = hue.Red | hue.Bold
+)
+
+// Logger is a command line logger. It is safe to use across concurrently
+// executing goroutines.
+type Logger struct {
+	w          io.Writer
+	timeFormat string
+	level      Level
+}
+
+// New returns a new [Logger].
+func New(w io.Writer, options ...Option) *Logger {
+	logger := &Logger{
+		w:          w,
+		level:      LevelInfo,
+		timeFormat: time.RFC3339,
+	}
+
+	for _, option := range options {
+		option(logger)
+	}
+
+	return logger
+}
+
+// Debug writes a debug level log line.
+func (l *Logger) Debug(msg string) {
+	l.log(LevelDebug, msg)
+}
+
+// Info writes an info level log line.
+func (l *Logger) Info(msg string) {
+	l.log(LevelInfo, msg)
+}
+
+// Warn writes a warning level log line.
+func (l *Logger) Warn(msg string) {
+	l.log(LevelWarn, msg)
+}
+
+// Error writes an error level log line.
+func (l *Logger) Error(msg string) {
+	l.log(LevelError, msg)
+}
+
+// log logs the given levelled message.
+func (l *Logger) log(level Level, msg string) {
+	if l.w == io.Discard || l.level > level {
+		// Do as little work as possible
+		return
+	}
+
+	// Buffer the output as e.g. stderr is not buffered by default. Do this
+	// by fetching and putting buffers from a [sync.Pool] so we don't have to
+	// constantly allocate new buffers
+	buf := getBuffer()
+	defer putBuffer(buf)
+
+	fmt.Fprintf(
+		buf,
+		"%s %s: %s\n",
+		timestampStyle.Text(time.Now().Format(l.timeFormat)),
+		level.styled(),
+		msg,
+	)
+
+	// WriteTo drains the buffer
+	buf.WriteTo(l.w) //nolint: errcheck // Just like printing
+}
+
+// Each log method (Debug, Info, Warn) etc.
+var bufPool = sync.Pool{
+	New: func() any {
+		return new(bytes.Buffer)
+	},
+}
+
+// getBuffer fetches a buffer from the pool, the returned buffer
+// is empty and ready to use.
+func getBuffer() *bytes.Buffer {
+	buf := bufPool.Get().(*bytes.Buffer) //nolint:revive,errcheck,forcetypeassert // We are in total control of this
+	buf.Reset()
+	return buf
+}
+
+// putBuffer puts the buffer back into the pool.
+func putBuffer(buf *bytes.Buffer) {
+	// Proper usage of a sync.Pool requires each entry to have approximately
+	// the same memory cost. To obtain this property when the stored type
+	// contains a variably-sized buffer, we add a hard limit on the maximum buffer
+	// to place back in the pool.
+	//
+	// See https://go.dev/issue/23199
+
+	// Approx 65kb
+	const maxSize = 64 << 10
+	if buf.Cap() > maxSize {
+		// Make the buffer nil so GC cleans it up
+		buf = nil
+	}
+
+	bufPool.Put(buf)
 }
