9077797bce79013421e74b2381a0cff3e765dc91
Tom Fleet <tomfleet2018@gmail.com>
2025-04-05 11:12:14 +0100

    Add `FromContext` and `WithContext` (#11)
    * Add `FromContext` and `WithContext`
    
    * Add tests for the missing logger case

--- log_test.go
+++ log_test.go
@@ -214,6 +214,46 @@ func TestRace(t *testing.T) {
 	test.Equal(t, len(lines), n*2, test.Context("expected %d log lines", n*2))
 }
 
+func TestContext(t *testing.T) {
+	t.Run("present", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+
+		// Constantly return the same time
+		fixedTime := func() time.Time {
+			fixed, err := time.Parse(time.RFC3339, "2025-04-01T13:34:03Z")
+			test.Ok(t, err)
+
+			return fixed
+		}
+
+		// Configure it a bit so we know we're getting the right one
+		logger := log.New(buf, log.TimeFunc(fixedTime), log.TimeFormat(time.Kitchen))
+
+		logger.Info("Before")
+
+		ctx := t.Context()
+
+		ctx = log.WithContext(ctx, logger)
+
+		after := log.FromContext(ctx)
+
+		after.Info("After")
+
+		got := buf.String()
+
+		test.Diff(t, got, "1:34PM INFO: Before\n1:34PM INFO: After\n")
+	})
+
+	t.Run("missing", func(t *testing.T) {
+		_, stderr := test.CaptureOutput(t, func() error {
+			log.FromContext(t.Context()).Info("FromContext")
+			return nil
+		})
+
+		test.True(t, strings.Contains(stderr, "FromContext"))
+	})
+}
+
 func BenchmarkLogger(b *testing.B) {
 	hue.Enabled(true) // Force colour
 
