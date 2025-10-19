934e7cc9619f63cc91d2f40ba9aa745841388b11
Tom Fleet <tomfleet2018@gmail.com>
2025-10-19 17:41:50 +0100

    Breaking: replace key value pairs with slog.Attrs (#27)
    * Breaking: replace key value pairs with slog.Attrs
    
    * Format README.md
    
    * Use slices.Concat

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/slog"
 	"math/rand/v2"
 	"os"
 	"time"
@@ -11,15 +12,23 @@ import (
 func main() {
 	logger := log.New(os.Stderr, log.WithLevel(log.LevelDebug))
 
-	logger.Debug("Searing steak", "cook", "rare", "temp", "42°C", "time", 2*time.Minute)
+	logger.Debug(
+		"Searing steak",
+		slog.String("cook", "rare"),
+		slog.Int("temp", 42),
+		slog.Duration("time", 2*time.Minute),
+	)
 	sleep()
-	logger.Info("Choosing wine pairing", "choices", []string{"merlot", "malbec", "rioja"})
+	logger.Info(
+		"Choosing wine pairing",
+		slog.Any("choices", []string{"merlot", "malbec", "rioja"}),
+	)
 	sleep()
 	logger.Error("No malbec left!")
 	sleep()
-	logger.Warn("Falling back to second choice", "fallback", "rioja")
+	logger.Warn("Falling back to second choice", slog.String("fallback", "rioja"))
 
-	logger.Info("Eating steak", "cut", "sirloin", "enjoying", true)
+	logger.Info("Eating steak", slog.String("cut", "sirloin"), slog.Bool("enjoying", true))
 }
 
 func sleep() {
